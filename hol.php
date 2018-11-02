485b94440239456ef827d9d3cdb73b3d754d1caa
khalil012 <elfatnikhalil@gmail.com>
2018-11-02 17:16:46 +0100

    primer com

---
+++ hol.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+
+
+
+
+<?php 
+
+$numero=1
+$nombbre="khalil"
+
+if (condition) {
+	echo "hola m";
+}
+
+public function hablar()
+{
+	# code...
+}
+
+
+ ?>
+
+
+ <?php 
+class miClase {
+
+    public $nombre="khalil";
+
+    public function miClase(){
+
+    }
+
+    public function  nombre($nombre){
+
+        $this->nombre=nombre;
+
+    }
+
+}
+
+
+
+  ?>
+
+
+</body>
+</html>
